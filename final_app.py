2dd38c3725c26fd7e7054910a0483113af11ac5b
Noel Jomichan <noel.jomichan@gmail.com>
2024-09-01 08:46:40 +0530

    updated README.md

--- final_app.py
+++ final_app.py
@@ -31,8 +31,6 @@ if 'email' not in st.session_state:
     st.session_state.email = ""
 if 'messages' not in st.session_state:
     st.session_state.messages = []
-if 'waiting_for_customer_id' not in st.session_state:
-    st.session_state.waiting_for_customer_id = False
 if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []  
 
